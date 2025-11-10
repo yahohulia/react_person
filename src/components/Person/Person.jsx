b4c04aba7b3fde734d95362a73744e283a50f4ab
yahohulia <yaroslavhohulia@gmail.com>
2025-11-10 19:40:00 +0200

    destructured person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,16 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age !== undefined ? (
-        <p className="Person__age">I am {person.age}</p>
-      ) : (
-        ''
-      )}
-      {person.isMarried === false ? (
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== undefined ? <p className="Person__age">I am {age}</p> : ''}
+      {isMarried === false ? (
         <p className="Person__partner">I am not married</p>
       ) : (
         <p className="Person__partner">
-          {`${person.partnerName} is my `}
-          {person.sex === 'f' ? `husband` : `wife`}
+          {`${partnerName} is my `}
+          {sex === 'f' ? `husband` : `wife`}
         </p>
       )}
     </section>
